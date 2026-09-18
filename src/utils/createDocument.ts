9ba233f00de45290961c91335c9889101a69feb7
Mark Jerwin <git@jrwnnnn.me>
2026-09-18 08:41:13 +0800

    refactor: Extract PDF setup into a helper

---
+++ src/utils/createDocument.ts
@@ -0,0 +1,14 @@
+import { initFonts } from "@utils/styleField";
+import { PDFDocument } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
+
+export async function createDocument(template: ArrayBuffer) {
+	const pdfDoc = await PDFDocument.load(template);
+	pdfDoc.registerFontkit(fontkit);
+	await initFonts(pdfDoc);
+
+	return {
+		pdfDoc,
+		acroform: pdfDoc.getForm(),
+	};
+}
