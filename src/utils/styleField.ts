b96bd675f0838b38bed00701e5c4c6b43b6ddc09
Mark Jerwin <git@jrwnnnn.me>
2026-09-22 22:16:10 +0800

    feat: Style PDF fields; add Bookman Old Style font support (#103)
    * feat: Style PDF fields; add Bookman Old Style font support
    
    * fix: Reduce form field font size to 7.8

--- src/utils/styleField.ts
+++ src/utils/styleField.ts
@@ -7,22 +7,41 @@ const alignmentMap = {
 	right: TextAlignment.Right,
 };
 
-export type FontName = "arial-bold" | "arial-narrow-bold";
+export type FontName =
+	| "arial-bold"
+	| "arial-narrow-bold"
+	| "bookman-old-style"
+	| "bookman-old-style-bold";
+
 let arialBoldBytes: ArrayBuffer | null = null;
 let arialNarrowBoldBytes: ArrayBuffer | null = null;
+let bookmanOldStyleBytes: ArrayBuffer | null = null;
+let bookmanOldStyleBoldBytes: ArrayBuffer | null = null;
+
 let fontMap: Record<FontName, PDFFont>;
 
 export async function initFonts(pdfDoc: PDFDocument): Promise<void> {
 	if (!arialBoldBytes || !arialNarrowBoldBytes) {
 		[arialBoldBytes, arialNarrowBoldBytes] = await Promise.all([
-			fetch("/fonts/Arial-Bold.ttf").then((r) => r.arrayBuffer()),
-			fetch("/fonts/Arial-Narrow-Bold.ttf").then((r) => r.arrayBuffer()),
+			fetch("/fonts/arial/bold.ttf").then((r) => r.arrayBuffer()),
+			fetch("/fonts/arial/narrow-bold.ttf").then((r) => r.arrayBuffer()),
+		]);
+	}
+
+	if (!bookmanOldStyleBytes || !bookmanOldStyleBoldBytes) {
+		[bookmanOldStyleBytes, bookmanOldStyleBoldBytes] = await Promise.all([
+			fetch("/fonts/bookman-old-style/regular.ttf").then((r) =>
+				r.arrayBuffer(),
+			),
+			fetch("/fonts/bookman-old-style/bold.ttf").then((r) => r.arrayBuffer()),
 		]);
 	}
 
 	fontMap = {
 		"arial-bold": await pdfDoc.embedFont(arialBoldBytes),
 		"arial-narrow-bold": await pdfDoc.embedFont(arialNarrowBoldBytes),
+		"bookman-old-style": await pdfDoc.embedFont(bookmanOldStyleBytes),
+		"bookman-old-style-bold": await pdfDoc.embedFont(bookmanOldStyleBoldBytes),
 	};
 }
 
