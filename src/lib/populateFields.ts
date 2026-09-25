b50a712d4ef7ea4eec2df98f263b2a7ca5b4a6b4
Mark Jerwin <git@jrwnnnn.me>
2026-09-25 19:09:54 +0800

    feat: Use style dictionary for PDF field styling (#111)
    * chore: Add `@data` path alias in tsconfig
    
    * feat: Use style dictionary for PDF field styling
    
    * Revert "feat: Update sf9-4-10.pdf fields"
    
    This reverts commit 828a01d02d7bced7304413ee30efb5a5c287a576.
    
    * refactor: Share alignment type across field styling

--- src/lib/populateFields.ts
+++ src/lib/populateFields.ts
@@ -1,6 +1,9 @@
-﻿import type { PDFForm } from "pdf-lib";
+﻿import type { Alignment } from "@utils/styleField.ts";
+import type { FontName } from "@utils/fonts";
+import type { PDFForm } from "pdf-lib";
 import { PDFName, PDFTextField } from "pdf-lib";
 import { styleField } from "@utils/styleField.ts";
+import styleDictionary from "@data/styleDictionary.json";
 
 export async function populateFields(
 	form: PDFForm,
@@ -23,6 +26,23 @@ export async function populateFields(
 			field.acroField.setDefaultAppearance("/Arial 12 Tf 0 g");
 		}
 
-		styleField(field, "bookman-old-style-bold", 7.8, "center");
+		const fieldStyle =
+			(
+				styleDictionary as Record<
+					string,
+					{
+						font?: FontName;
+						fontSize?: number;
+						alignment?: Alignment;
+					}
+				>
+			)[field.getName()] ?? {};
+
+		styleField(
+			field,
+			fieldStyle.font ?? "bookman-old-style-bold",
+			fieldStyle.fontSize ?? 7.8,
+			fieldStyle.alignment ?? "center",
+		);
 	}
 }
